4c0c9bf813894b00909bd9eff16c7aa45772bbb4
General14 <haydeneastman@icloud.com>
2021-06-18 11:36:24 +0100

    Update script.js

--- Profile-Page/script.js
+++ Profile-Page/script.js
@@ -1,21 +1,37 @@
 debug.console
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+function showQuestions(questions, quizContainer){
+	// we'll need a place to store the output and the answer choices
+	var output = [];
+	var answers;
 
-	function showQuestions(questions, quizContainer){
-		// code will go here
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+		
+		// first reset the list of answers
+		answers = [];
+
+		// for each available answer to this question...
+		for(letter in questions[i].answers){
+
+			// ...add an html radio button
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		// add this question and its answers to the output
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
 	}
 
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
-	}
-
-	// show the questions
-	showQuestions(questions, quizContainer);
-
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
 }
 var myQuestions = [
 	{
@@ -37,3 +53,4 @@ var myQuestions = [
 		correctAnswer: 'a'
 	}
 ];
+showQuestions(questions, quizContainer);
