ccfdea268ce687556e06bbddfcedaa850c8bc8c0
General14 <haydeneastman@icloud.com>
2021-07-03 09:11:48 +0100

    d

--- Profile-Page/script.js
+++
@@ -1,8 +0,0 @@
-function clickAction() {
-    score.textContent = '0';
-    if (indiaCorrect.checked) {
-    score.textContent = Number(score.textContent) + 1;
-    
-  }
-}
-  button.onclick = clickAction;
