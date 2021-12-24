8b682e2bc1492e999a5b25248583c6c482dd76d7
General14 <haydeneastman@icloud.com>
2021-12-24 10:56:03 +0000

    dsas

--- London/script.js
+++ London/script.js
@@ -1,7 +1,4 @@
-//Get the button:
 mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -11,9 +8,7 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
